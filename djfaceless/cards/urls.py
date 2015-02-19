4a530b0f5e23542a0db94a6a9954027b628f5636
furalyon <ramkishore123@gmail.com>
2015-02-19 11:49:33 +0530

    added simple template file and changed view function into generic class-base listview

--- djfaceless/cards/urls.py
+++ djfaceless/cards/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from djfaceless.cards import views
 
-urlpatterns = patterns('djfaceless.cards.views',
-    url(r'^$', 'browse', name='browse'),
+urlpatterns = patterns('',
+    url(r'^$', views.BrowseView.as_view(), name='browse'),
 )
--- djfaceless/cards/views.py
+++ djfaceless/cards/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from djfaceless.cards.models import Card
 
-def browse(request):
-    return render(request, 'cards/browse.html', {
+#install django-filters for filtering
 
-    })
+#use Paginator class. It has all the stuff I need
+from django.core.paginator import Paginator
+
+class BrowseView(ListView):
+    queryset = Card.objects.collectible()
+    template_name = 'cards/browse.html'
+    paginate_by = 20
